197fa7ef6b9b421ddbd552db909ecb54334bde44
北住ケイスケ <kitazumikeisuke@kitazumikeisukenoiMac.local>
2022-02-13 09:16:29 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+const NameText = (props) => {
+  return (
+    <p>hi,{props.name}!</p>
+  )
+}
+
+const Profile = (props) => {
+  return (
+    <div>
+      <p>{props.age}</p>
+      <p>{props.country}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <NameText name=" Jack" />
+      <Profile age={22} country="US" />
+      <NameText name="Mary" />
+      <Profile age={20} country="UK" />
+    </div>
+  )
+}
+
+export default App;
