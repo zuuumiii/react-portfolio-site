8bde382491d3468cfc9c7b6ed0515ee1edded78a
北住ケイスケ <kitazumikeisuke@kitazumikeisukenoiMac.local>
2022-02-13 23:09:04 +0900

    create skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,41 @@
+import { Circle } from 'react-circle';
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+import { requestStates } from '../constants';
+
 export const Skills = () => {
+  const [state, dispatch] = useReducer(skillReducer, initialState);
+  useEffect(() => {
+    dispatch({ type: actionTypes.fetch });
+    axios.get('https://api.github.com/users/zuuumiii/repos')
+      .then((response) => {
+        const languageList = response.data.map(res => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
+  }, []);
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  };
+
+  const converseCountToPercentage = (count) => {
+    if (count > 10) { return 100; }
+    return count * 10;
+  };
+
+
   return (
     <div id='Skills'>
       <div className='container'>
@@ -6,6 +43,30 @@ export const Skills = () => {
           <h2>Skills</h2>
         </div>
         <div className="skills-container">
+          {
+            state.requestState === requestStates.loading && (
+              <p className='description'>取得中</p>
+            )
+          }
+
+          {
+            state.requestState === requestStates.success && (
+              state.languageList.map((item, index) => (
+                <div className='skill-item' key={index}>
+                  <p className='description'><strong>{item.language}</strong></p>
+                  <Circle
+                    animate
+                    progress={converseCountToPercentage(item.count)}
+                  />
+                </div>
+              )))
+          }
+
+          {
+            state.requestState === requestStates.error && (
+              <p className='description'>エラーが発生しました</p>
+            )
+          }
 
         </div>
       </div>
---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const requestStates = {
+  idle: 'idle',
+  loading: 'LOADING',
+  success: 'SUCCESS',
+  error: 'ERROR',
+};
+
